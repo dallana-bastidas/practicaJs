179b2a28dca34ab608867566f8d65b8dfcd719d4
Dallana Bastidas <cindydallana2822@gmail.com>
2024-11-13 15:09:22 -0500

    onKeyUp

--- piano2/code.js
+++ piano2/code.js
@@ -12,18 +12,44 @@
 
  */
 
-window.onload = () => {
-  var keys = document.querySelectorAll(".key");
-  keys.forEach(function (key) {
-    key.addEventListener("click", function () {
-      var note = this.getAttribute("data-note");
-      playNote(note);
-    });
-  });
+// onclick
 
-  function playNote(note){
-      var audio = new Audio('audio/nota1.mp3');
-      audio.play()
+// window.onload = () => {
+//   var keys = document.querySelectorAll(".key");
+//   keys.forEach(function (key) {
+//     key.addEventListener("click", function () {
+//       var note = this.getAttribute("data-note");
+//       playNote(note);
+//     });
+//   });
+
+//   function playNote(note){
+//       var audio = new Audio('audio/nota1.mp3');
+//       audio.play()
+//   }
+
+// onkeyup
+
+function playSound(note) {
+  const audio = new Audio("audio/nota1.mp3");
+  audio.play();
+}
+
+document.addEventListener("keyup", (event) => {
+  const keyMap = {
+    a: "Do",
+    s: "Re",
+    d: "Mi",
+    f: "Fa",
+    g: "Sol",
+    h: "La",
+    j: "Si",
+  };
+  const note = keyMap[event.key];
+  if (note) {
+    playSound(note);
+    const keyElement = document.querySelector('#pianoKeyUp .key[data-note="${note}"]');
+    keyElement.classList.add("active");
+    setTimeout(() => keyElement.classList.remove("active"), 200);
   }
-
-};
+});
