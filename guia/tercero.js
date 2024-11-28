12ce509e3eb166742e7e6565f301089bf5087fa5
Dallana Bastidas <cindydallana2822@gmail.com>
2024-11-28 16:27:01 -0500

    ejercicio 3

--- guia/tercero.js
+++ guia/tercero.js
@@ -1,3 +1,16 @@
 // Anagramas
 // Escribe una función que determine si dos cadenas son anagramas (contienen las mismas
 // letras en diferente orden).
+
+function anagramas(cadena1, cadena2) {
+  const limpia = (cadena) => cadena.toLowerCase().replace(/\s+/g, "");
+
+  const odenado = (cadena) => limpia(cadena).split("").sort().join("");
+
+  return odenado(cadena1) === ordenado(cadena2);
+}
+
+console.log(anagramas("amor", "roma"));
+console.log(anagramas("anagrama", "nagrama"));
+console.log(anagramas("hola", "adios"));
+console.log("españa", "se pana");
