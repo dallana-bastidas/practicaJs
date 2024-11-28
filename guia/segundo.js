0d776fc95f56a804994424488dac076658d02da4
Dallana Bastidas <cindydallana2822@gmail.com>
2024-11-28 16:15:12 -0500

    ejercicio dos

--- guia/segundo.js
+++ guia/segundo.js
@@ -1,3 +1,15 @@
 // Palíndromo
 // Escribe una función que determine si una cadena es un palíndromo (se lee igual de
 // izquierda a derecha que de derecha a izquierda).
+
+function palindromo(cadena) {
+  const limpia = cadena.toLowerCase().replace(/\s+/g, "");
+
+  const invertida = limpia.split("").reverse().join("");
+
+  return limpia === invertida;
+}
+
+console.log(palindromo("anita lava la tina"));
+console.log("hola mundo");
+console.log("amo la paloma");
